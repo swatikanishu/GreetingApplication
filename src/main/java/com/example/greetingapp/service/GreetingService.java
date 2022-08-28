4e48d4d3e8db4ac04fed368bc8529ee8c73224e6
swatikanishu <nishuswatika@gmail.com>
2022-08-28 14:33:46 +0530

    Ability for the grreting app to give message

--- src/main/java/com/example/greetingapp/service/GreetingService.java
+++ src/main/java/com/example/greetingapp/service/GreetingService.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService{
     public static String greetingMessage() {
-        return "Hello World!!! :)";
+        return "Hello World";
     }
 
     public static String greetMessageWithUser(String firstName, String lastName) {
         if (firstName.isEmpty() && lastName.isEmpty()) {
             return greetingMessage();
         } else if (lastName.equals("") && !firstName.equals("")) {
-            return "Hello! " + firstName + ", Welcome to the BridgeLabz!";
+            return "Hello! " + firstName + ", Welcome to the  Lucknow!";
         } else if (!lastName.equals("") && firstName.equals("")) {
             return "Hello! " + lastName + ", Welcome to the BridgeLabz!";
         }
-        return "Hello! " + firstName +" "+ lastName + ", Welcome to the BridgeLabz!";
+        return "Hello! " + firstName +" "+ lastName + ", Welcome to the GreetingApp!";
     }
 }
